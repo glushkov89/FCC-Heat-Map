6f403715b79397b82007756f6e81dcbe64a73c9a
glushkov89 <kellerman117@gmail.com>
2018-08-27 14:49:13 +0300

    Legend axis left.

--- script/app.js
+++ script/app.js
@@ -31,6 +31,7 @@
 // Remember to use the Read-Search-Ask method if you get stuck.
 
 document.addEventListener("DOMContentLoaded", function() {
+	const d3 = require("d3");
 	const req = new XMLHttpRequest();
 	req.open(
 		"GET",
@@ -41,15 +42,13 @@ document.addEventListener("DOMContentLoaded", function() {
 	req.onload = function() {
 		const json = JSON.parse(req.responseText);
 		const data = json.monthlyVariance;
-		console.log(data);
-		//		window.innerWidth and .innerHeight
-		console.log(`${window.innerHeight}x${window.innerWidth}`);
+
 		/*----------------D3 Code here-----------------*/
-		const d3 = require("d3");
-		const mg = { t: 50, b: 50, r: 30, l: 70 };
-		/*-----------------Attributes------------------*/
+
+		//Margins
+		const mg = { t: 70, b: 90, r: 30, l: 70 };
 		//Bar
-		const b = { h: 20, w: 3 };
+		const b = { h: 30, w: 3 };
 		//Diag
 		const d = {
 			h: 12 * b.h,
@@ -61,53 +60,82 @@ document.addEventListener("DOMContentLoaded", function() {
 			w: mg.l + d.w + mg.r,
 			i: "svg-canvas"
 		};
-		console.log(d);
+		//COLORBREWER
+		const clr = [
+			"#a50026",
+			"#d73027",
+			"#f46d43",
+			"#fdae61",
+			"#fee090",
+			"#ffffbf",
+			"#e0f3f8",
+			"#abd9e9",
+			"#74add1",
+			"#4575b4",
+			"#313695"
+		];
+		//Legend
+		const lg = { h: 15, w: 30 };
+		//Month names for y-axis
+		const mon = [
+			"January",
+			"February",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+			"August",
+			"September",
+			"October",
+			"November",
+			"December"
+		];
 
-		//Scales
-		const parseMonth = d3.timeParse("%m");
-		const parseYear = d3.timeParse("%Y");
-		// let minMax = json
-		// 	.map((obj) => +obj.Time.substring(0, 2))
-		// 	.sort((a, b) => a - b);
+		/*-----------Scales----------*/
 		const x = d3
-			.scaleTime()
-			.domain([
-				d3.min(data, (d) => parseYear(d.year + 1)),
-				d3.max(data, (d) => parseYear(d.year - 1))
-			])
+			.scaleBand()
+			.domain(
+				data.reduce((acc, obj) => {
+					acc.includes(obj.year) ? null : acc.push(obj.year);
+					return acc;
+				}, [])
+			)
 			.range([0, d.w]);
-		//console.log(d3.max(data, (d) => parseYear(d.year + 1)));
-		//console.log(d3.min(data, (d) => parseYear(d.year - 1)));
+
 		const y = d3
-			.scaleTime()
-			.domain([
-				d3.max(data, (d) => parseMonth(d.month)),
-				d3.min(data, (d) => parseMonth(d.month))
-			])
+			.scaleBand()
+			.domain(d3.range(1, 13))
 			.range([0, d.h]);
-		//console.log(y(parseMonth(1)));
-		//console.log(y(parseMonth(12)));
-		console.log(x.domain());
 
-		//Axis
-		const yAxis = d3.axisLeft(y).tickFormat(d3.timeFormat("%B"));
-		//			.tickSize(1, 10);
-		const xAxis = d3.axisBottom(x);
+		const color = d3
+			.scaleQuantize()
+			.domain([
+				d3.min(data, (d) => d.variance + json.baseTemperature),
+				d3.max(data, (d) => d.variance + json.baseTemperature)
+			])
+			.range(clr.reverse());
 
+		/*------------SVG + Diagram------------*/
 		const svg = d3
 			.select("main")
 			.append("svg")
 			.attr("id", s.i)
 			.attr("height", s.h)
 			.attr("width", s.w);
-
 		const diag = svg
 			.append("g")
 			.attr("height", d.h)
 			.attr("width", d.w)
 			.attr("transform", `translate(${mg.l},${mg.t})`);
 
-		/*---------Generating axis`s-----------*/
+		/*-------------Axis----------------*/
+		const yAxis = d3.axisLeft(y).tickFormat((n, i) => mon[i]);
+		const xAxis = d3
+			.axisBottom(x)
+			.tickValues(x.domain().filter((d, i) => !(i % 10)));
+
+		//Generating axis`s
 		const myYaxs = diag
 			.append("g")
 			.call(yAxis)
@@ -118,27 +146,121 @@ document.addEventListener("DOMContentLoaded", function() {
 			.attr("id", "x-axis")
 			.attr("transform", `translate(0,${d.h})`);
 
-		// let tooltipDiv = d3
-		// 	.select("body")
-		// 	.append("div")
-		// 	.attr("id", "tooltip")
-		// 	.attr("class", "mytooltip")
-		// 	.style("opacity", 0);
+		/*----------Tooltip------------*/
+		let tooltipDiv = d3
+			.select("body")
+			.append("div")
+			.attr("id", "tooltip")
+			.attr("class", "mytooltip")
+			.attr("data-year", d.year)
+			.style("opacity", 0);
 
-		// const title = svg
-		// 	.append("text")
-		// 	.attr("id", "title")
-		// 	.text("Doping in Professional Bicycle Racing");
+		const tooltipParse = (obj) =>
+			`${obj.year} - ${mon[obj.month - 1]}</br>${d3.format(".1f")(
+				obj.variance + json.baseTemperature
+			)}&deg;C</br>${d3.format(".1f")(obj.variance)}&deg;C`;
 
-		// /*---Centering title in data and top margin---*/
-		// title
-		// 	.attr(
-		// 		"x",
-		// 		mg.left +
-		// 			diaAt.width / 2 -
-		// 			title.node().getBoundingClientRect().width / 2
-		// 	)
-		// 	.attr("y", (mg.top + title.node().getBoundingClientRect().height) / 2);
+		/*----------Rectangles------------*/
+		diag
+			.selectAll("rect")
+			.data(data)
+			.enter()
+			.append("rect")
+			.attr("data-month", (d) => d.month - 1)
+			.attr("data-year", (d) => d.year)
+			.attr("data-temp", (d) => d.variance)
+			.attr("class", "cell")
+			.attr("x", (d) => x(d.year))
+			.attr("y", (d) => y(d.month))
+			.attr("height", b.h)
+			.attr("width", b.w)
+			.attr("fill", (d) => color(d.variance + json.baseTemperature))
+			.on("mouseover", (d) => {
+				tooltipDiv
+					.attr("data-year", d.year)
+					.transition()
+					.duration(100)
+					.style("opacity", 0.9);
+				tooltipDiv
+					.html(tooltipParse(d))
+					.style("left", d3.event.pageX + 10 + "px")
+					.style("top", d3.event.pageY - 80 + "px");
+			})
+			.on("mouseout", () => {
+				tooltipDiv
+					.transition()
+					.duration(300)
+					.style("opacity", 0);
+			});
+
+		/*----------Title + Description------------*/
+		const title = svg
+			.append("text")
+			.attr("id", "title")
+			.text("Monthly Global Land-Surface Temperature");
+
+		const description = svg
+			.append("text")
+			.attr("id", "description")
+			.html(
+				`${d3.min(x.domain())}-${d3.max(x.domain())}, base temperature - ${
+					json.baseTemperature
+				}&deg;C`
+			);
+
+		//Centering title and description in data and top margin
+		title
+			.attr(
+				"x",
+				mg.l + d.w / 2 - title.node().getBoundingClientRect().width / 2
+			)
+			.attr("y", title.node().getBoundingClientRect().height / 2 + 20);
+		description
+			.attr(
+				"x",
+				mg.l + d.w / 2 - description.node().getBoundingClientRect().width / 2
+			)
+			.attr(
+				"y",
+				title.node().getBoundingClientRect().height +
+					description.node().getBoundingClientRect().height +
+					10
+			);
+
+		/*----------Legend------------*/
+		const legend = svg.append("g").attr("id", "legend");
+		legend
+			.selectAll("rect")
+			.data(clr)
+			.enter()
+			.append("rect")
+			.attr("x", (d, i) => i * lg.w)
+			.attr("height", lg.h)
+			.attr("width", lg.w)
+			.attr("fill", (d) => d)
+			.attr("stroke", "black");
+
+		console.log(
+			color.range().map((d) => {
+				d = color.invertExtent(d);
+				if (d[0] == null) d[0] = x.domain()[0];
+				if (d[1] == null) d[1] = x.domain()[1];
+				return d;
+			})
+		);
+
+		const tmp = myXaxs.node().getBoundingClientRect().height;
+
+		legend.attr(
+			"transform",
+			`translate(${mg.l +
+				(d.w - legend.node().getBoundingClientRect().width) / 2},${mg.t +
+				d.h +
+				tmp +
+				(mg.b - tmp) / 2 -
+				legend.node().getBoundingClientRect().height / 2})`
+		);
+
 		// /*Label for y-axis*/
 		// const yLabel = diag
 		// 	.append("text")
@@ -155,40 +277,6 @@ document.addEventListener("DOMContentLoaded", function() {
 		// /*Circles*/
 		// const color = d3.scaleOrdinal(d3.schemeCategory10).domain([0, false, true]);
 
-		// const tooltipParse = (obj) =>
-		// 	`${obj.Name} (${obj.Nationality})</br>${obj.Year} Place ${obj.Place} in ${
-		// 		obj.Time
-		// 	} min</br>${obj.Doping}`;
-		// diag
-		// 	.selectAll("rect")
-		// 	.data(json)
-		// 	.enter()
-		// 	.append("circle")
-		// 	.attr("data-xvalue", (d) => parseYear(d.Year))
-		// 	.attr("data-yvalue", (d) => parseMinSec(d.Time))
-		// 	.attr("class", "dot")
-		// 	.attr("fill", (d) => color(d.Doping === ""))
-		// 	.style("stroke", "black")
-		// 	.attr("cx", (d) => x(parseYear(d.Year)))
-		// 	.attr("cy", (d) => y(parseMinSec(d.Time)))
-		// 	.attr("r", (d) => (d.Doping ? 4 : 8))
-		// 	.on("mouseover", (d) => {
-		// 		tooltipDiv
-		// 			.attr("data-year", parseYear(d.Year))
-		// 			.transition()
-		// 			.duration(100)
-		// 			.style("opacity", 0.9);
-		// 		tooltipDiv
-		// 			.html(tooltipParse(d))
-		// 			.style("left", d3.event.pageX + mg.right / 2 + "px")
-		// 			.style("top", d3.event.pageY - mg.bottom + "px");
-		// 	})
-		// 	.on("mouseout", () => {
-		// 		tooltipDiv
-		// 			.transition()
-		// 			.duration(300)
-		// 			.style("opacity", 0);
-		// 	});
 		// /*Legend*/
 		// const lmg = { t: 2, b: 2, r: 2, l: 2 };
 		// const lrec = { h: 15, w: 15 };
